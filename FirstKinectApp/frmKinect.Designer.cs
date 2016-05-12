b64808aff8f43da66301909a22b7624b1dae8760
dmsloan <dsloan@ibe-inc.com>
2016-05-11 19:55:02 -0700

    First comit of First Kinect App

---
+++ FirstKinectApp/frmKinect.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace FirstKinectApp
+{
+    partial class frmKinect
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStream = new System.Windows.Forms.Button();
+            this.lblTextStatus = new System.Windows.Forms.Label();
+            this.lblTextConnectionID = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblConnectionID = new System.Windows.Forms.Label();
+            this.KinectStream = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.KinectStream)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStream
+            // 
+            this.btnStream.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStream.Location = new System.Drawing.Point(573, 578);
+            this.btnStream.Name = "btnStream";
+            this.btnStream.Size = new System.Drawing.Size(88, 25);
+            this.btnStream.TabIndex = 0;
+            this.btnStream.Text = "Stream";
+            this.btnStream.UseVisualStyleBackColor = true;
+            this.btnStream.Click += new System.EventHandler(this.btnStream_Click);
+            // 
+            // lblTextStatus
+            // 
+            this.lblTextStatus.AutoSize = true;
+            this.lblTextStatus.Location = new System.Drawing.Point(12, 9);
+            this.lblTextStatus.Name = "lblTextStatus";
+            this.lblTextStatus.Size = new System.Drawing.Size(40, 13);
+            this.lblTextStatus.TabIndex = 1;
+            this.lblTextStatus.Text = "Status:";
+            // 
+            // lblTextConnectionID
+            // 
+            this.lblTextConnectionID.AutoSize = true;
+            this.lblTextConnectionID.Location = new System.Drawing.Point(14, 54);
+            this.lblTextConnectionID.Name = "lblTextConnectionID";
+            this.lblTextConnectionID.Size = new System.Drawing.Size(78, 13);
+            this.lblTextConnectionID.TabIndex = 2;
+            this.lblTextConnectionID.Text = "Connection ID:";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(93, 9);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(10, 13);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "-";
+            // 
+            // lblConnectionID
+            // 
+            this.lblConnectionID.AutoSize = true;
+            this.lblConnectionID.Location = new System.Drawing.Point(93, 54);
+            this.lblConnectionID.Name = "lblConnectionID";
+            this.lblConnectionID.Size = new System.Drawing.Size(10, 13);
+            this.lblConnectionID.TabIndex = 4;
+            this.lblConnectionID.Text = "-";
+            // 
+            // KinectStream
+            // 
+            this.KinectStream.Location = new System.Drawing.Point(17, 70);
+            this.KinectStream.Name = "KinectStream";
+            this.KinectStream.Size = new System.Drawing.Size(640, 480);
+            this.KinectStream.TabIndex = 5;
+            this.KinectStream.TabStop = false;
+            // 
+            // frmKinect
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 615);
+            this.Controls.Add(this.KinectStream);
+            this.Controls.Add(this.lblConnectionID);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblTextConnectionID);
+            this.Controls.Add(this.lblTextStatus);
+            this.Controls.Add(this.btnStream);
+            this.Name = "frmKinect";
+            this.Text = "Kinect";
+            ((System.ComponentModel.ISupportInitialize)(this.KinectStream)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStream;
+        private System.Windows.Forms.Label lblTextStatus;
+        private System.Windows.Forms.Label lblTextConnectionID;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblConnectionID;
+        private System.Windows.Forms.PictureBox KinectStream;
+    }
+}
+
---
+++ FirstKinectApp/frmKinect.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Microsoft.Kinect;
+
+//using Coding4Fun.Kinect.WinForm;
+ 
+namespace FirstKinectApp
+{
+    public partial class frmKinect : Form
+    {
+        private KinectSensor kSensor;
+
+        public frmKinect()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStream_Click(object sender, EventArgs e)
+        {
+            if (btnStream.Text == "Stream")
+            {
+                if (KinectSensor.KinectSensors.Count > 0)
+                {
+                    this.btnStream.Text = "Stop";
+                    kSensor = KinectSensor.KinectSensors[0];
+                    KinectSensor.KinectSensors.StatusChanged += KinectSensors_StatusChanged;
+                }
+
+                kSensor.Start();
+                this.lblConnectionID.Text = kSensor.DeviceConnectionId;
+                kSensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+               kSensor.ColorFrameReady += KSensor_ColorFrameReady;
+            }
+            else
+            {
+                if (kSensor != null && kSensor.IsRunning)
+                {
+                    kSensor.Stop();
+                    this.btnStream.Text = "Stream";
+//                    //this.
+                }
+            }
+        }
+
+        private void KSensor_ColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (var frame = e.OpenColorImageFrame())
+            {
+                if (frame != null)
+                {
+                    KinectStream.Image = CreateBitmapFromSensor(frame);
+                }
+            }
+        }
+
+        private void KinectSensors_StatusChanged(object sender, StatusChangedEventArgs e)
+        {
+            this.lblStatus.Text= kSensor.Status.ToString();
+        }
+
+        private Bitmap CreateBitmapFromSensor(ColorImageFrame frame)
+        {
+            var pixelData = new byte[frame.PixelDataLength];
+            frame.CopyPixelDataTo(pixelData);
+ 
+            var stride = frame.Width * frame.BytesPerPixel;
+            var bmpFrame = new Bitmap(frame.Width, frame.Height, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+            var bmpData = bmpFrame.LockBits(new Rectangle(0, 0, frame.Width, frame.Height), System.Drawing.Imaging.ImageLockMode.WriteOnly, bmpFrame.PixelFormat);
+
+            System.Runtime.InteropServices.Marshal.Copy(pixelData, 0, bmpData.Scan0, frame.PixelDataLength);
+
+            bmpFrame.UnlockBits(bmpData);
+            return bmpFrame;
+
+        }
+    }
+}
